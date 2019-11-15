dc18f9df9fcb461eadb9031cbd24b80506d97854
Ryan <ryan@rmiller.dev>
2019-11-15 14:22:48 -0500

    sorting linked list

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,109 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private Node head;
+
+    public void add(T element) {
+        if(head == null) {
+            head = new Node(element);
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = new Node(element);
+        }
+    }
+
+    public void remove(Integer index) {
+        if(index.equals(0)) {
+            head = head.next;
+        } else {
+            Node n = head;
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            n.next = n.next.next;
+        }
+    }
+
+    public Boolean contains(T element) {
+        for (Node n = head; n != null; n = n.next) {
+            if (n.data.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer find(T element) {
+        Node n = head;
+        for (int i = 0; n != null; n = n.next, i++) {
+            if (n.data.equals(element)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public Integer size() {
+        int i = 0;
+        for (Node n = head; n != null; n = n.next) {
+            i++;
+        }
+        return i;
+    }
+
+    public T get(Integer index) {
+        Node n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.next;
+        }
+        return n.data;
+    }
+
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> copy = new SinglyLinkedList<T>();
+
+        for (Node n = head; n != null; n = n.next) {
+            copy.add(n.data);
+        }
+
+        return  copy;
+    }
+
+    public void sort(Comparator<T> comparator) {
+        SinglyLinkedList<T> sorted = new SinglyLinkedList<T>();
+
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+
+            T smallest = head.data;
+            for (Node n = head; n != null; n = n.next) {
+                if(comparator.compare(smallest, n.data) >= 0) {
+                    smallest = n.data;
+                }
+            }
+            this.remove(this.find(smallest));
+            sorted.add(smallest);
+        }
+        this.head = sorted.head;
+    }
+
+
+    private class Node {
+        private Node next;
+        private T data;
+
+        public Node() {}
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,146 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+
+    @Test
+    public void testAdd() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+
+        Assert.assertEquals(3, list.get(0).intValue());
+        Assert.assertEquals(10, list.get(1).intValue());
+    }
+
+    @Test
+    public void testRemove() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        Assert.assertEquals(15, list.get(2).intValue());
+        list.remove(2);
+        Assert.assertEquals(2, list.get(2).intValue());
+
+        Assert.assertEquals(3, list.get(0).intValue());
+        list.remove(0);
+        Assert.assertEquals(10, list.get(0).intValue());
+        Assert.assertEquals(3, list.size().intValue());
+    }
+
+    @Test
+    public void testContains() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        Assert.assertTrue(list.contains(3));
+        Assert.assertTrue(list.contains(-14));
+        Assert.assertTrue(list.contains(15));
+
+        Assert.assertFalse(list.contains(213));
+
+    }
+
+    @Test
+    public void testFind() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        Assert.assertEquals( 0, list.find(3).intValue() );
+        Assert.assertEquals( 2, list.find(15).intValue() );
+        Assert.assertEquals( 4, list.find(-14).intValue() );
+
+    }
+
+    @Test
+    public void testSize() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+
+        Assert.assertEquals(5, list.size().intValue());
+    }
+
+    @Test
+    public void testGet() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        Assert.assertEquals(3, list.get(0).intValue());
+        Assert.assertEquals(10, list.get(1).intValue());
+        Assert.assertEquals(2, list.get(3).intValue());
+        Assert.assertEquals(-14, list.get(4).intValue());
+
+    }
+
+    @Test
+    public void testClone() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        SinglyLinkedList<Integer> newList = list.copy();
+
+        Assert.assertEquals(3, newList.get(0).intValue());
+        Assert.assertEquals(10, newList.get(1).intValue());
+        Assert.assertEquals(2, newList.get(3).intValue());
+        Assert.assertEquals(-14, newList.get(4).intValue());
+    }
+
+
+    @Test
+    public void testSort() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+
+        list.add(3);
+        list.add(10);
+        list.add(15);
+        list.add(2);
+        list.add(-14);
+
+        list.sort(Comparator.<Integer>naturalOrder());
+
+        Assert.assertEquals(-14, list.get(0).intValue());
+        Assert.assertEquals(2, list.get(1).intValue());
+        Assert.assertEquals(10, list.get(3).intValue());
+        Assert.assertEquals(15, list.get(4).intValue());
+    }
 }
